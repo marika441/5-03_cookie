29ea9e0eb789bc9f63b3ece707e60487aa9c20fa
marika441 <92457238+marika441@users.noreply.github.com>
2022-02-16 23:11:06 +0900

    [Add]cookie

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const agree = Cookies.get('cookie-agree');
+if(agree === 'yes') {
+    console.log('クッキーを確認しました');
+} else {
+    console.log('クッキーを確認できません');
+    document.getElementById('agreebtn').onclick = function() {
+        Cookies.set('cookie-agree', 'yes', {expires: 7});
+    };
+}
